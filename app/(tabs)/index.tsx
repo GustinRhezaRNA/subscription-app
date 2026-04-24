7af046c01da23e4fe83b0fe87159943f7387b6c9
Gustin Rheza <rezarna4@gmail.com>
2026-04-24 08:31:21 +0700

    feat: fully develop home page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,26 +1,82 @@
 import "@/global.css";
-import { Link } from "expo-router";
-import { Text } from "react-native";
-
+import { FlatList, Image, Text, View } from "react-native";
 import { styled } from "nativewind";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+import images from "@/constants/images";
+import { HOME_BALANCE, HOME_SUBSCRIPTIONS, HOME_USER, UPCOMING_SUBSCRIPTIONS } from "@/constants/data";
+import { icons } from "@/constants/icons";
+import { formatCurrency } from "@/lib/utils";
+import dayjs from "dayjs";
+import ListHeading from "@/components/ListHeading";
+import UpcomingSubscriptionCard from "@/components/UpcomingSubscriptionCard";
+import SubscriptionCard from "@/components/SubscriptionCard";
+import { useState } from "react";
 const SafeAreaView = styled(RNSafeAreaView);
 
 export default function App() {
+
+  const [expandedSubscriptionId, setExpandedSubscriptonId] = useState<string | null>(null);
+
+
   return (
-    <SafeAreaView className="flex-1 bg-background font-sans-extrabold p-5">
-      <Text className="font-bold text-primary text-7xl">
-        Home
-      </Text>
-      <Link href="./onboarding" className="mt-4 font-sans-bold rounded bg-primary text-white p-4">
-        <Text className="text-accent">Go to Onboarding</Text>
-      </Link>
-      <Link href="./(auth)/sign-in" className="mt-4 font-sans-bold rounded bg-primary text-white p-4">
-        <Text className="text-accent">Go to Sign In</Text>
-      </Link>
-      <Link href="./(auth)/sign-up" className="mt-4 font-sans-bold rounded bg-primary text-white p-4">
-        <Text className="text-accent">Go to Sign Up</Text>
-      </Link>
+    <SafeAreaView className="flex-1 bg-background p-5">
+      <FlatList
+        ListHeaderComponent={
+          () => (
+            <>
+              <View className="home-header">
+
+                <View className="home-user">
+                  <Image source={images.avatar} className="home-avatar"></Image>
+                  <Text className="home-user-name">{HOME_USER.name}</Text>
+                </View>
+                <Image source={icons.add} className="home-add-icon"></Image>
+              </View>
+
+              <View className="home-balance-card">
+                <Text className="home-balance-label" >Balance</Text>
+                <View className="home-balance-row">
+                  <Text className="home-balance-amount"> {formatCurrency(HOME_BALANCE.amount)}</Text>
+                  <Text className="home-balance-date">{dayjs(HOME_BALANCE.nextRenewalDate).format('MM/DD')}</Text>
+                </View>
+              </View>
+
+              <View className="mb-5">
+                <ListHeading title="Upcoming" />
+                <FlatList data={UPCOMING_SUBSCRIPTIONS} renderItem={({ item }) => (
+                  <UpcomingSubscriptionCard {...item} />
+                )} keyExtractor={(item) => item.id}
+                  horizontal
+                  showsHorizontalScrollIndicator={false}
+                  ListEmptyComponent={
+                    <Text className="home-empty-state">
+                      No Upcoming Renewals Yet
+                    </Text>
+                  }
+                />
+              </View>
+
+              <ListHeading title="All Subscriptions" />
+            </>
+          )
+        }
+        data={HOME_SUBSCRIPTIONS}
+        renderItem={({ item }) => (
+          <SubscriptionCard {...item}
+            expanded={expandedSubscriptionId === item.id}
+            onPress={() => setExpandedSubscriptonId(expandedSubscriptionId === item.id ? null : item.id)}
+          />
+        )} keyExtractor={(item) => item.id}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={
+          <Text className="home-empty-state">
+            No Subscriptions Found
+          </Text>
+        }
+        extraData={expandedSubscriptionId}
+        ItemSeparatorComponent={() => <View className="h-4"></View>}
+        contentContainerClassName="pb-20"
+      />
     </SafeAreaView>
   );
 }
